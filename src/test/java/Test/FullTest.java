05c37f8b26ab6eb54699f3b4416cf2ffd5f7b3f3
shahosx <shahosx@gmail.com>
2017-07-27 23:52:58 -0400

    Automated with Screenshot

---
+++ src/test/java/Test/FullTest.java
@@ -0,0 +1,167 @@
+package Test;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class FullTest {
+	
+WebDriver driver;
+
+
+@BeforeTest
+public void set() {
+	driver =new FirefoxDriver();
+	driver.get("http://www.demoaut.com");
+}
+@Test (priority =0)
+public void testLunch(){
+	launch goLaunch = PageFactory.initElements(driver, launch.class);
+	 String GetTitle = goLaunch.getTitle();
+	 Assert.assertTrue(GetTitle.contains("Welcome: "));
+}
+@Test(priority =1)
+public void testPage1() {
+	
+	goRegister registerForm =PageFactory.initElements(driver, goRegister.class);
+	registerForm.AllRegisterApp("Shahadat", "Hossain", "3478632195", "abc123@bd.com", "1 Ave a", "Apt 3", "Brooklyn","New York","11218","UNITED STATES","abc123", "Password1", "Password1");
+
+	String VerifyRegisterForm = registerForm.VerifyConfirmRegister();
+	Assert.assertTrue(VerifyRegisterForm.contains("Thank you for registering."));
+}
+@Test (priority= 3)
+public void testSignin ()	 {
+	 	 
+	
+	 SignOn goSignOn= PageFactory.initElements(driver, SignOn.class);
+	 goSignOn.SignIn_AllMethods("mercury", "mercury");
+	
+	 String VerifyLoggedInPage = goSignOn.LoginVerify();
+	 Assert.assertTrue(VerifyLoggedInPage.contains("SIGN-OFF"));
+}
+
+@Test (priority =4)
+public void testBookFlight () {
+	BookAticket goBookAticket =PageFactory.initElements(driver, BookAticket.class);
+	goBookAticket.setAllFlightFinder();
+	goBookAticket.setAllSelectFlight();
+	goBookAticket.setAllBookAflight("Shahadat", "Hossain", "123456789123456", "Shahadat", "H", "Hossain");
+	
+	String VerifyBookConfirmationx = goBookAticket.getVersion();
+	Assert.assertTrue(VerifyBookConfirmationx.contains("(v."));
+	String verifyFlightConfirmation = goBookAticket.getFlightConfirmation();
+	Assert.assertTrue(verifyFlightConfirmation.contains("Your itinerary"));
+}
+@Test (priority=5)
+public void testSupport () {
+	Support goSupport =PageFactory.initElements(driver, Support.class);
+	goSupport.ClickSupport();
+	String verifyVersion = goSupport.getVersion();
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+
+}
+@Test (priority=6)
+public void testContact () {
+	Contact goContact =PageFactory.initElements(driver, Contact.class);
+	goContact.ClickContact();
+	String verifyVersion = goContact.getVersion();
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+
+}
+
+@Test (priority=2)
+public void testHome () {
+	Home goHome =PageFactory.initElements(driver, Home.class);
+	goHome.ClickHome();
+	String verifyVersion = goHome.getVersion();
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+
+}
+@Test (priority=7)
+public void testHotels () {
+	Hotels goHotels =PageFactory.initElements(driver, Hotels.class);
+	goHotels.ClickHotels();
+	String verifyVersion = goHotels.getVersion();
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+
+}
+@Test (priority=8)
+public void testCarRentals () {
+	CarRentals goCarRentals =PageFactory.initElements(driver, CarRentals.class);
+	goCarRentals.ClickCarRentals();
+	String verifyVersion = goCarRentals.getVersion();
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+}
+@Test (priority=9)
+public void testCruises () {
+	Cruises goCruises =PageFactory.initElements(driver, Cruises.class);
+	goCruises.ClickCruises();
+	goCruises.clickCruisesReservation();
+	
+//	String verifyCruises = goCruises.getCruisesReservation();
+//	Assert.assertTrue(verifyCruises.contains("Cruise Itinerary"));
+	goCruises.clickItinerary();
+	goCruises.clickCancelTicket();
+
+	String verifyItinararyPage = goCruises.getVersion();
+	Assert.assertTrue(verifyItinararyPage.contains("(v."));
+	
+	
+}
+@Test (priority=10)
+public void testDestinations () {
+	Destinations goDestinations =PageFactory.initElements(driver, Destinations.class);
+	goDestinations.ClickDestinations();
+	String verifyVersion = goDestinations.getVersion();
+	System.out.println("I am before last Case 10 YAY!");
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+
+}
+@Test (priority=11)
+public void testVacations () {
+	Vacations goVacations =PageFactory.initElements(driver, Vacations.class);
+	goVacations.ClickVacations();
+	String verifyVersion = goVacations.getVersion();
+	System.out.println("I am last Case YAY!");
+	System.out.println("Current web applicatin version is :" + verifyVersion);
+	Assert.assertTrue(verifyVersion.contains("(v."));
+
+}
+
+@AfterMethod
+public void tearDown(ITestResult result)
+{
+if(ITestResult.FAILURE==result.getStatus())
+{
+try 
+{
+TakesScreenshot ts=(TakesScreenshot)driver;
+File source=ts.getScreenshotAs(OutputType.FILE);
+FileUtils.copyFile(source, new File("./Screenshotss/"+result.getName()+".png"));
+System.out.println("Screenshot taken");
+} 
+catch (Exception e)
+{
+
+System.out.println("Exception while taking screenshot "+e.getMessage());
+}}}
+}
+
+
